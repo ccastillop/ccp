1fc228a9439e8c5b56ed88b2e6e8af7e13ed8ebe
ccastillop <ccastillop@gmail.com>
2013-03-03 20:32:54 -0500

    database

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,35 +13,39 @@ server "cristiancastillo.com", :web, :app, :db, primary: true
 default_run_options[:pty] = true
 ssh_options[:forward_agent] = true
 
+set :rvm_ruby_string, '1.9.3'
+
 require "bundler/capistrano"
 require 'capistrano-unicorn'
 require "rvm/capistrano"
 
 before 'deploy:setup', 'rvm:install_rvm'   # install RVM
+before 'deploy:setup', 'rvm:install_ruby'
 
 after 'deploy:restart', 'unicorn:reload' # app IS NOT preloaded
 after 'deploy:restart', 'unicorn:restart'  # app preloaded
 after "deploy", "deploy:cleanup" # keep only the last 5 releases
 
-task :setup_config, roles: :app do
-  sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
-  run "mkdir -p #{shared_path}/config"
-  put File.read("config/database.example.yml"), "#{shared_path}/config/database.yml"
-  puts "Now edit the config files in #{shared_path}."
-end
 after "deploy:setup", "deploy:setup_config"
-
-task :symlink_config, roles: :app do
-  run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
-end
 after "deploy:finalize_update", "deploy:symlink_config"
+before "deploy", "deploy:check_revision"
 
-desc "Make sure local git is in sync with remote."
-task :check_revision, roles: :web do
-  unless `git rev-parse HEAD` == `git rev-parse origin/master`
-    puts "WARNING: HEAD is not the same as origin/master"
-    puts "Run `git push` to sync changes."
-    exit
+namespace :deploy do
+  task :setup_config, roles: :app do
+    sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
+    run "mkdir -p #{shared_path}/config"
+    put File.read("config/database.example.yml"), "#{shared_path}/config/database.yml"
+    puts "Now edit the config files in #{shared_path}."
+  end
+  task :symlink_config, roles: :app do
+    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+  end
+  desc "Make sure local git is in sync with remote."
+  task :check_revision, roles: :web do
+    unless `git rev-parse HEAD` == `git rev-parse origin/master`
+      puts "WARNING: HEAD is not the same as origin/master"
+      puts "Run `git push` to sync changes."
+      exit
+    end
   end
 end
-before "deploy", "deploy:check_revision"
